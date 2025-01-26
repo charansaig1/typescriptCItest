617c57c9c74f5d0c2f8219c5a17b02b5960df711
charansaig1 <charansaig1@gmail.com>
2025-01-26 12:53:21 +0530

    otp

--- otp.sh
+++ otp.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-# Prompt the user for OTP length
-echo "Enter the OTP length (4 or 6):"
-read length
+# Generate a random 6-digit number (OTP)
+otp=$(shuf -i 100000-999999 -n 1)
 
-# Function to generate a random OTP
-generate_otp() {
-  local otp_length=$1
-
-  # Check if the entered length is valid
-  if [[ $otp_length -ne 4 && $otp_length -ne 6 ]]; then
-    echo "Error: Invalid input. Please enter 4 or 6."
-    exit 1
-  fi
-
-  # Generate a random OTP using /dev/urandom and print it
-  local otp=$(cat /dev/urandom | tr -dc '0-9' | fold -w $otp_length | head -n 1)
-  echo "Generated OTP: $otp"
-}
-
-# Call the function with the user's input
-generate_otp $length
+# Print the OTP
+echo "Generated OTP: $otp"
